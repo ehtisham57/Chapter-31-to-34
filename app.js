00a253fd8663017041fbbfe081f452574378d193
ehtisham57 <81214666+ehtisham57@users.noreply.github.com>
2021-11-15 12:32:03 -0800

    not completed

---
+++ app.js
@@ -0,0 +1,44 @@
+//==============DATE METHODS============
+//Qestion 1
+// var a = new Date()
+// console.log(a)
+
+
+//Question 2
+
+
+// var a = new Date()
+// var b = a.getMonth();
+// console.log(b + 1)
+
+
+
+//Question 3
+
+// var a = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var b = new Date();
+// var c = b.getDay();
+// var d = a[c];
+// console.log(d)
+
+//Question 4
+// var a = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+// var b = prompt("Enter your day").toLowerCase()
+// if (b === a[0, 6]) {
+//     document.write("Its Fun Day")
+// } else
+//     document.write("Its Working Day")
+
+
+//Question 5
+
+
+// var b = new Date()
+// var c = b.getDate()
+// if (c <= 15) {
+//     document.write("First day's of month")
+// } else
+//     document.write("Last day's Of Month")
+
+
+//Question 6
